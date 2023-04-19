e076a34d399873e7a06138b0c052546df8ad46e1
checonunez74 <checonunez@gmail.com>
2023-04-18 19:04:24 -0500

    finish setting up the fireabase

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+// Import the functions you need from the SDKs you need
+const firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/database');
+require('firebase/storage');
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBc9Got5jj1EPes-we3Xt_KDgRU278JPSc",
+  authDomain: "sergion-icv.firebaseapp.com",
+  projectId: "sergion-icv",
+  storageBucket: "sergion-icv.appspot.com",
+  messagingSenderId: "484001833609",
+  appId: "1:484001833609:web:d7b5000c2bc0d826999c58",
+  measurementId: "G-F8DMGSS4FX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
+//////////////////////////////////////Firebase Above/////////////////////////////////////////////////////
+
+
 // Full Documentation - https://www.turbo360.co/docs
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
@@ -9,7 +46,7 @@ const config = {
 	db: vertex.nedbConfig((process.env.TURBO_ENV=='dev') ? 'nedb://'+path.join(__dirname, process.env.TMP_DIR) : 'nedb://'+process.env.TMP_DIR)
 }
 
-const app = vertex.app(config) // initialize app with config options
+//const app = vertex.app(config) // initialize app with config options
 
 // order matters here:
 app.use(vertex.fetchGlobal(process.env.TURBO_API_KEY, process.env.TURBO_ENV, turbo)) // fetch global config on every route
